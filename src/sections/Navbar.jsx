645dd8785991188002ccd395744cedd94db3282f
JulienC_W97 <julien.cauquis@gmail.com>
2025-11-28 19:51:37 +0100

    Working on navbar for small screens

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { motion } from "motion/react";
 import { HashLink } from 'react-router-hash-link';
+import classNames from "classnames";
 
 function Navigation({ onLinkClick }){
     return (
@@ -21,31 +22,49 @@ function Navigation({ onLinkClick }){
 const Navbar = () => {
     const[isOpen, setIsOpen] = useState(false);
     return (
-        <div className="fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40">
+        <div className={`fixed inset-x-0 z-20 w-full ${!isOpen ? 'backdrop-blur-lg bg-primary/40' : ''}`}>
             <div className="mx-auto c-space max-w-7xl">
                 <div className="flex items-center justify-between py-2 sm:py-0">
                     <a href="/" className="text_xl font-bold transition-colors text-neutral-400 hover:text-white">
                         Me
                     </a>
                     <button onClick={() => setIsOpen(!isOpen)} className="flex cursor-pointer text-neutral-400 hover:text-white focus:outline-none sm:hidden">
-                        <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} className="w-6 h-6" alt="toggle" />
+                        <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} className="w-6 h-6 z-40" alt="toggle" />
                     </button>
                     <nav className="hidden sm:flex">
                         <Navigation />
                     </nav>
                 </div>
             </div>
-            {isOpen && (<motion.div 
+            {isOpen && (
+                <div
+                    className="backdrop-blur-lg bg-primary/40"
+                    style={{
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100vh',
+                        zIndex: 30,
+                        backgroundColor: 'color-mix(in oklab, var(--color-primary) /* #030412 */ 40%, transparent)',
+                    }}
+                    onClick={() => {
+                    setIsOpen(false);
+                    }}
+                >                    
+                <motion.div 
                         className="block overflow-hidden text-center sm:hidden"
                         initial={{ opacity: 0, x: -10}}
                         animate={{ opacity: 1, x: 0}}
                         style={{ maxHeight: "100vh"}}
                         transition={{ duration: 1 }}
-                        >
-                <nav className="pb-5">
-                    <Navigation onLinkClick={() => (setIsOpen(false))} />
-                </nav>
-            </motion.div>
+                        //onClick={(e) => e.stopPropagation()}
+                >
+                    <nav className="pb-5" >
+                        <Navigation onLinkClick={() => (setIsOpen(false))} />
+                    </nav>
+                </motion.div>
+                </div>
             )}
         </div>
     );
